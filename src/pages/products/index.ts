350346a554f4d2c9fd7a84503985bdf0f88f02b9
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-04-13 14:58:50 -0300

    feat: add authentication route

--- src/pages/products/index.ts
+++ src/pages/products/index.ts
@@ -1,5 +1,6 @@
 import { makeProductList } from "~/app/main/factories/pages";
+import { useAuthenticated } from "~/app/presentation/hooks";
 
 export default function ProductListPage() {
-  return makeProductList()
+  return useAuthenticated(makeProductList);
 }
--- src/pages/products/new/index.ts
+++ src/pages/products/new/index.ts
@@ -1,5 +1,7 @@
+// import { useRouter } from "next/router";
 import { makeNewProductForm } from "~/app/main/factories/pages";
+import { useAuthenticated } from "~/app/presentation/hooks";
 
 export default function NewProductFormPage() {
-  return makeNewProductForm()
+  return useAuthenticated(makeNewProductForm);
 }
