d6f4704da29365e82aa3c6f53582690eaaa2f215
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-04-13 14:42:37 -0300

    refactor: remove authorize-http-get-client-decorator-factory

--- src/app/main/factories/decorators/index.ts
+++ src/app/main/factories/decorators/index.ts
@@ -1,3 +1,2 @@
-export * from './authorize-http-get-client-decorator-factory';
 export * from './authorize-http-post-client-decorator-factory';
 export * from './remote-authentication-decorator-factory';
